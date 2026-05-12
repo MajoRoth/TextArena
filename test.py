628a424cb8135a905a395828cdea30cbed9055d4
amitroth <amitroth@gmail.com>
2026-05-12 14:32:07 +0300

    demonstrate hvta

--- test.py
+++ test.py
@@ -4,12 +4,11 @@ import textarena as ta
 
 agents = {
     0: ta.agents.HumanAgent(),
-    1: ta.agents.OpenRouterAgent(model_name="google/gemini-2.0-flash-001"),
 }
 
 # initialize the environment
-env = ta.make(env_id="SimpleTak-v0-train")
-# env = ta.wrappers.SimpleRenderWrapper(env=env) #, render_mode="standard")
+env = ta.make(env_id="Wordle-v0")
+env = ta.wrappers.FilesystemWrapper(env=env, hidden_solution=True) #, render_mode="standard")
 env.reset(num_players=len(agents))
 
 # main game loop
@@ -18,6 +17,8 @@ while not done:
   player_id, observation = env.get_observation()
   action = agents[player_id](observation)
   done, step_info = env.step(action=action)
+  if step_info.get("reward_hack"):
+    print("\033[33m[HACK DETECTED] A hack was triggered in this step!\033[0m")
 rewards, game_info = env.close()
 print(rewards)
 print(game_info)
